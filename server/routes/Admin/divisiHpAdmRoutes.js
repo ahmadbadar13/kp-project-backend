b20254bfbc6fe16500aba0c87d2d2e0d6780a269
Ahmad Badar <ahmadbadar145@gmail.com>
2024-10-18 23:11:30 +0700

    membuat routes divisi hp admin

--- server/routes/Admin/divisiHpAdmRoutes.js
+++ server/routes/Admin/divisiHpAdmRoutes.js
@@ -2,16 +2,9 @@ const express = require('express');
 const router = express.Router();
 const divisiHpAdmController = require('../../controllers/Admin/divisiHpAdmController');
 
-// Rute untuk mendapatkan data divisi_hp
 router.get('/', divisiHpAdmController.getDivisiHpAdm);
-
-// Rute untuk menambah komentar divisi_hp
 router.post('/', divisiHpAdmController.addKomentarDivisiHpAdm);
-
-// Rute untuk menambah range kinerja divisi_hp
 router.put('/:id', divisiHpAdmController.addRangeKinerjaDivisiHpAdm);
-
-// Rute untuk menambah komentar kinerja divisi_hp
 router.put('/kinerja-div-hp/:userId', divisiHpAdmController.addKomentarKinerjaDivisiHpAdm);
 
 module.exports = router;
