053a4b2208066038e7535c1d2ff8a84036504549
Ahmad Badar <ahmadbadar145@gmail.com>
2024-10-23 13:13:35 +0700

    perbaikan fungsi getStrukturOrganisasi

--- server/controllers/strukturOrganisasiController.js
+++ server/controllers/strukturOrganisasiController.js
@@ -7,14 +7,24 @@ exports.getStrukturOrganisasi = (req, res) => {
         'SELECT foto_div_pdi AS foto, nama_div_pdi AS nama, "Anggota" AS peran FROM divisi_pdi LIMIT 1',
         'SELECT foto_div_hp AS foto, nama_div_hp AS nama, "Anggota" AS peran FROM divisi_hp LIMIT 1',
         'SELECT foto_div_sppp_sdm AS foto, nama_div_sppp_sdm AS nama, "Anggota" AS peran FROM divisi_sppp_sdm LIMIT 1',
+        
+        'SELECT foto_sekretaris AS foto, nama_sekretaris AS nama, "Sekretaris" AS peran FROM sekretaris LIMIT 1',
+        
+        'SELECT foto_sb_hsdm AS foto, nama_sb_hsdm AS nama, nip_sb_hsdm AS nip, posisi_sb_hsdm AS posisi, "Sub Bagian HSDM" AS sub_bagian FROM sub_bagian_hsdm',
+        'SELECT foto_sb_kul AS foto, nama_sb_kul AS nama, nip_sb_kul AS nip, posisi_sb_kul AS posisi, "Sub Bagian KUL" AS sub_bagian FROM sub_bagian_kul',
+        'SELECT foto_sb_pdi AS foto, nama_sb_pdi AS nama, nip_sb_pdi AS nip, posisi_sb_pdi AS posisi, "Sub Bagian PDI" AS sub_bagian FROM sub_bagian_pdi',
+        'SELECT foto_sb_tppph AS foto, nama_sb_tppph AS nama, nip_sb_tppph AS nip, posisi_sb_tppph AS posisi, "Sub Bagian TPPPH" AS sub_bagian FROM sub_bagian_tppph'
     ];
 
     Promise.all(queries.map((query) => new Promise((resolve, reject) => {
         db.query(query, (error, results) => {
-        if (error) return reject(error);
-        resolve(results[0]);
+            if (error) return reject(error);
+            resolve(results);
         });
     })))
-    .then(results => res.json(results))
+    .then(results => {
+        const mergedResults = [].concat(...results);
+        res.json(mergedResults);
+    })
     .catch(err => res.status(500).json({ error: err.message }));
 };
