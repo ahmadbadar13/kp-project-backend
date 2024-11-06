b2d2ba539e8f91bfdbd41843e83cbd6819a6e38a
Ahmad Badar <ahmadbadar145@gmail.com>
2024-11-06 14:44:01 +0700

    ubah menjadi class

--- server/controllers/strukturOrganisasiController.js
+++ server/controllers/strukturOrganisasiController.js
@@ -1,15 +1,21 @@
 const StrukturOrganisasi = require('../models/strukturOrganisasiModel');
 
-exports.getStrukturOrganisasi = (req, res) => {
-    Promise.all([
-        new Promise((resolve, reject) => StrukturOrganisasi.getKetua((err, result) => (err ? reject(err) : resolve(result)))),
-        new Promise((resolve, reject) => StrukturOrganisasi.getAnggotaDivisi((err, result) => (err ? reject(err) : resolve(result)))),
-        new Promise((resolve, reject) => StrukturOrganisasi.getSekretaris((err, result) => (err ? reject(err) : resolve(result)))),
-        new Promise((resolve, reject) => StrukturOrganisasi.getSubBagian((err, result) => (err ? reject(err) : resolve(result))))
-    ])
-    .then(results => {
-        const mergedResults = [].concat(...results);
-        res.json(mergedResults);
-    })
-    .catch(err => res.status(500).json({ error: err.message }));
-};
+class StrukturOrganisasiController {
+    static async getStrukturOrganisasi(req, res) {
+        try {
+            const results = await Promise.all([
+                new Promise((resolve, reject) => StrukturOrganisasi.getKetua((err, result) => (err ? reject(err) : resolve(result)))),
+                new Promise((resolve, reject) => StrukturOrganisasi.getAnggotaDivisi((err, result) => (err ? reject(err) : resolve(result)))),
+                new Promise((resolve, reject) => StrukturOrganisasi.getSekretaris((err, result) => (err ? reject(err) : resolve(result)))),
+                new Promise((resolve, reject) => StrukturOrganisasi.getSubBagian((err, result) => (err ? reject(err) : resolve(result))))
+            ]);
+            
+            const mergedResults = [].concat(...results);
+            res.json(mergedResults);
+        } catch (err) {
+            res.status(500).json({ error: err.message });
+        }
+    }
+}
+
+module.exports = StrukturOrganisasiController;
