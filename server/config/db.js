494f28d837ce1dd38d72c8edbd3fab520080b141
Ahmad Badar <ahmadbadar145@gmail.com>
2024-11-12 23:33:17 +0700

    menggunakan database hosting

--- server/config/db.js
+++ server/config/db.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql');
 
 // Konfigurasi koneksi database
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'db_project_kpu'
-});
-
 // const db = mysql.createConnection({
-//     host: 'bz4kzerb13skvzsklu2n-mysql.services.clever-cloud.com',
-//     user: 'uteofvtkhgj76a2o',
-//     password: 'WW8UkNEM2x3438ppvsUv',
-//     database: 'bz4kzerb13skvzsklu2n'
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'db_project_kpu'
 // });
+
+const db = mysql.createConnection({
+    host: 'bbjyah058htu46i4dpdw-mysql.services.clever-cloud.com',
+    user: 'ulezqu8v6xvuqbso',
+    password: 'BreZTvluF7LxIrfsIjVG',
+    database: 'bbjyah058htu46i4dpdw'
+});
 
 db.connect((err) => {
     if (err) {
